8f69a9f492ff048a1c7d2e63df305038e93eb875
Jose <josealejandrogilmendez08@gmail.com>
2023-02-09 15:10:07 -0400

    create a function that creates a random fleet

--- src/modules/player.js
+++ src/modules/player.js
@@ -32,6 +32,44 @@ class Player {
     return check;
   };
 
+  // creates a ship with random pos and orientation
+  randomShip = (length) => {
+    const pos1 = Math.floor(Math.random() * 10);
+    const pos2 = Math.floor(Math.random() * 10);
+    let dir = Math.round(Math.random());
+
+    if (dir === 0) {
+      dir = 'h';
+      if (this.gameboard.placeShip(pos1, pos2, length, dir) === false) return false;
+    }
+
+    if (dir === 1) {
+      dir = 'v';
+      if (this.gameboard.placeShip(pos1, pos2, length, dir) === false) return false;
+    }
+
+    return true;
+  };
+
+  randomFleet = () => {
+    // create 1 ships of length 1
+    for (let i = 0; i < 1;) {
+      if (this.randomShip(1) !== false) i += 1;
+    }
+    // create 1 ships of length 2
+    for (let i = 0; i < 1;) {
+      if (this.randomShip(2) !== false) i += 1;
+    }
+    // create 1 ships of length 3
+    for (let i = 0; i < 1;) {
+      if (this.randomShip(3) !== false) i += 1;
+    }
+    // create 2 ship of length 4
+    for (let i = 0; i < 2;) {
+      if (this.randomShip(4) !== false) i += 1;
+    }
+  };
+
   // return {
   //   gameboard, changeTurn, getTurn, attack, randomAttack,
   // };
